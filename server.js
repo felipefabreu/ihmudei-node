de2b1060833fd69ba3db3052c9978c5cd80f640b
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2019-07-05 12:48:11 -0300

    Configurando primeiro schema do product, adicionando requireDir e mongoose

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//Iniciando o app
 const app = express();
 
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+//Primeira rota
 app.get('/', (req, res) => {
-    res.send("Hello Felipe");
+    Product.create({
+        title: "React",
+        description: "Build apps with react",
+        url: "http://github.com/facebook/react",
+    });
+    
+    return res.send("Hello Felipe");
  });
 
 app.listen(3001);
